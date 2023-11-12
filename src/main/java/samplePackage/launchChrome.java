c0e006894d60c57ac3fe3c30ba3f321520605415
Ferno <Ferno@LAPTOP-AE85928Q>
2023-11-12 11:58:52 +0530

    First commit

---
+++ src/main/java/samplePackage/launchChrome.java
@@ -0,0 +1,21 @@
+package samplePackage;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class launchChrome {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "D:\\Testing\\selenium\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://tutorialsninja.com/demo/index.php?route=common/home");
+		driver.manage().window().maximize();
+		
+		//driver.findElement(By.className("//span[@class='hidden-xs hidden-sm hidden-md' and contains(text(),'Currency')]")).click();
+		//driver.findElement(By.xpath("//button[@data-toggle='dropdown' and @class='btn btn-link dropdown-toggle']")).click();
+		driver.findElement(By.cssSelector("#form-currency button[class='btn btn-link dropdown-toggle']")).click();
+		driver.findElement(By.cssSelector("#top-links i[class='fa fa-phone']")).click();
+		}
+
+	}
+
