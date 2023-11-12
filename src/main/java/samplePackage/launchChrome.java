0d60687d30ab7f69b3b2ac70119137fe9deb3228
Fernandas006 <Fernandas006@gmail.com>
2023-11-12 22:07:05 +0530

    Update launchChrome.java

--- src/main/java/samplePackage/launchChrome.java
+++ src/main/java/samplePackage/launchChrome.java
@@ -11,10 +11,10 @@ public class launchChrome {
 		driver.get("https://tutorialsninja.com/demo/index.php?route=common/home");
 		driver.manage().window().maximize();
 		
-		//driver.findElement(By.className("//span[@class='hidden-xs hidden-sm hidden-md' and contains(text(),'Currency')]")).click();
+		driver.findElement(By.className("//span[@class='hidden-xs hidden-sm hidden-md' and contains(text(),'Currency')]")).click();
 		//driver.findElement(By.xpath("//button[@data-toggle='dropdown' and @class='btn btn-link dropdown-toggle']")).click();
 		//driver.findElement(By.cssSelector("#form-currency button[class='btn btn-link dropdown-toggle']")).click();
-		driver.findElement(By.cssSelector("#top-links i[class='fa fa-phone']")).click();
+		//driver.findElement(By.cssSelector("#top-links i[class='fa fa-phone']")).click();
 		}
 
 	}
